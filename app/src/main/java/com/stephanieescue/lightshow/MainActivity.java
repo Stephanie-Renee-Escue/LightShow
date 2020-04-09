24994fd8373ea3d786364c52239f67160e4788b1
Stephanie-Renee-Escue <sescue@my.apsu.edu>
2020-04-09 13:39:44 -0500

    Layout creation including landscape views

--- app/src/main/java/com/stephanieescue/lightshow/MainActivity.java
+++ app/src/main/java/com/stephanieescue/lightshow/MainActivity.java
@@ -2,24 +2,20 @@ package com.stephanieescue.lightshow;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import java.util.EventListener;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener, EventListener {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
-    }
-
-    @Override
-    public void onClick(View view) {
 
     }
 }
